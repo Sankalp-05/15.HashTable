6f2c993f9beacd909ed04f66cfbc1343a859b15f
Sankalp <sankalp.kulkarni05@gmail.com>
2023-01-24 15:55:45 +0530

    [Sankalp]- Welcome msg

---
+++ 15.HashTable/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _15.HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Hash Table Program ");
+        }
+    }
+}
